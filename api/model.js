6a0e00777e53365e109730db2b517d3aa2bcbc57
daviszung <daviszung@gmail.com>
2023-06-30 20:54:46 -0400

    fix git

--- api/model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userModel = void 0;
-exports.userModel = {
-    "username": "string",
-    "password": "string",
-    "dateCreated": "date", // Date when the user account was created (auto-generated)
-};
